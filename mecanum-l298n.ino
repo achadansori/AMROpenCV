a998a3c5594168633ed9f316059c18d5edb61f55
Muhammad Achad Ansori <94736530+achadansori@users.noreply.github.com>
2024-01-21 14:03:05 +0700

    Add files via upload

---
+++ mecanum-l298n.ino
@@ -0,0 +1,197 @@
+//MECANUM WHEELS ROBOT
+//ROBOT LK
+
+#define IN_11  22        // L298N #1 in 4 motor Front Right
+#define IN_12  24        // L298N #1 in 3 motor Front Right
+#define IN_13  26        // L298N #1 in 2 motor Front Left
+#define IN_14  28        // L298N #1 in 1 motor Front Left
+
+#define IN_21  30        // L298N #2 in 4 motor Back Left
+#define IN_22  32       // L298N #2 in 3 motor Back Left
+#define IN_23  34       // L298N #2 in 2 motor Back Right
+#define IN_24  36      // L298N #2 in 1 motor Back Right
+
+//ROBOT LK
+
+
+int command;             //Int to store app command state.
+boolean state = 1;
+
+
+void stopRobot() {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, LOW);
+
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, LOW);
+  
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, LOW);
+  
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, LOW);
+  
+}
+void back() {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, HIGH);
+
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, HIGH);
+ 
+  digitalWrite(IN_24, HIGH);
+  digitalWrite(IN_23, LOW);
+  
+  digitalWrite(IN_22, HIGH);
+  digitalWrite(IN_21, LOW);
+ 
+}
+void forward () {
+  digitalWrite(IN_11, HIGH);
+  digitalWrite(IN_12, LOW);
+
+  digitalWrite(IN_13, HIGH);
+  digitalWrite(IN_14, LOW);
+  
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, HIGH);
+ 
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, HIGH);
+ 
+}
+void left() {
+  digitalWrite(IN_11, HIGH);
+  digitalWrite(IN_12, LOW);
+  
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, HIGH);
+  
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, HIGH);
+  
+  digitalWrite(IN_22, HIGH);
+  digitalWrite(IN_21, LOW);
+  
+}
+void right() {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, HIGH);
+  
+  digitalWrite(IN_13, HIGH);
+  digitalWrite(IN_14, LOW);
+
+  digitalWrite(IN_24, HIGH);
+  digitalWrite(IN_23, LOW);
+ 
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, HIGH);
+  
+}
+void superleft () {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, HIGH);
+  
+  digitalWrite(IN_13, HIGH);
+  digitalWrite(IN_14, LOW);
+
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, HIGH);
+
+  digitalWrite(IN_22, HIGH);
+  digitalWrite(IN_21, LOW);
+ 
+}
+void superright () {
+  digitalWrite(IN_11, HIGH);
+  digitalWrite(IN_12, LOW);
+ 
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, HIGH);
+ 
+  digitalWrite(IN_24, HIGH);
+  digitalWrite(IN_23, LOW);
+ 
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, HIGH);
+
+}
+void forwardright () {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, LOW);
+
+  digitalWrite(IN_13, HIGH);
+  digitalWrite(IN_14, LOW);
+
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, LOW);
+
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, HIGH);
+  
+}
+void forwardleft () {
+  digitalWrite(IN_11, HIGH);
+  digitalWrite(IN_12, LOW);
+  
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, LOW);
+  
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, HIGH);
+ 
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, LOW);
+  
+}
+void backleft () {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, HIGH);
+
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, LOW);
+ 
+  digitalWrite(IN_24, HIGH);
+  digitalWrite(IN_23, LOW);
+  
+  digitalWrite(IN_22, LOW);
+  digitalWrite(IN_21, LOW);
+  
+}
+void backright () {
+  digitalWrite(IN_11, LOW);
+  digitalWrite(IN_12, LOW);
+  
+  digitalWrite(IN_13, LOW);
+  digitalWrite(IN_14, HIGH);
+  
+  digitalWrite(IN_24, LOW);
+  digitalWrite(IN_23, LOW);
+  
+  digitalWrite(IN_22, HIGH);
+  digitalWrite(IN_21, LOW);
+  
+}
+void setup() {
+  Serial.begin (9600);
+  
+  pinMode(IN_11, OUTPUT);
+  pinMode(IN_12, OUTPUT);
+  pinMode(IN_13, OUTPUT);
+  pinMode(IN_14, OUTPUT);
+
+  pinMode(IN_21, OUTPUT);
+  pinMode(IN_22, OUTPUT);
+  pinMode(IN_23, OUTPUT);
+  pinMode(IN_24, OUTPUT);
+
+   
+}
+//ROBOT LK
+
+void loop() {
+
+  stopRobot();
+  delay(2000);
+}
+//robot lk
